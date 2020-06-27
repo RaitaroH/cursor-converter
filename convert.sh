e97d00e7eb058397f8cd78a27682abd8e6deda2b
RaitaroHikami <19552720+RaitaroH@users.noreply.github.com>
2020-06-27 17:41:14 +0300

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,33 +1,20 @@
+#!/bin/bash
 rm -r outputs
 mkdir -p outputs
-cd outputs
 
-for name in ../inputs/*.ani; do
-    name=$(basename $name .ani)
-    mkdir -p $name
-    cd $name
-    cp ../../inputs/$name.ani .
-    ../../ani2ico/ani2ico $name.ani
-    rm $name.ani
-    for f in $name*.ico; do
-        filename=$(basename "$f")
-        png="${filename%.*}".png
-        convert $f $png
-        identify -format '%w 1 1 %f 200\n' $png >> $name.xcg
+for f in inputs/*.ani; do
+    name=$(basename $f .ani)
+    mkdir -p outputs/$name
+    cp "inputs/$name.ani" outputs/$name/
+    ani2ico/ani2ico "outputs/$name/$name.ani"
+    rm "outputs/$name/$name.ani"
+    cd "outputs/$name/"
+    for ff in *.ico; do
+        filename=$(basename $ff .ico)
+        convert "$filename.ico" "$filename.png"
+        identify -format '%w 1 1 %f 200\n' "$filename.png" >> "$filename.xcg"
+        xcursorgen "$filename.xcg" "$filename"
     done
-    xcursorgen $name.xcg $name
-    cd ..
+    rm *.ico
+    cd ../..
 done
-
-for name in ../inputs/*.cur; do
-    name=$(basename $name .cur)
-    mkdir -p $name
-    cd $name
-    cp ../../inputs/$name.cur .
-    convert $name.cur $name.png
-    identify -format '%w 1 1 %f\n' $name.png >> $name.xcg
-    xcursorgen $name.xcg $name
-    cd ..
-done
-
-cd ..
